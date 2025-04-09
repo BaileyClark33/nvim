39f2c4bc17782e2a452adddc61b0c7db491135d2
BaileyClark33 <bwc339@gmail.com>
2025-04-09 16:35:42 -0400

    Tacocat basic config

--- lua/plugins/misc.lua
+++ lua/plugins/misc.lua
@@ -1,29 +1,32 @@
 -- Standalone plugins with less than 10 lines of config go here
 return {
-	{
-		-- Tmux & split window navigation
-		"christoomey/vim-tmux-navigator",
-	},
-	{
-		-- Powerful Git integration for Vim
-		"tpope/vim-fugitive",
-		config = function()
-			vim.keymap.set("n", "<leader>gs", vim.cmd.Git)
-		end,
-	},
-	{
-		-- GitHub integration for vim-fugitive
-		"tpope/vim-rhubarb",
-	},
-	{
-		-- Hints keybinds
-		"folke/which-key.nvim",
-	},
-	{
-		-- High-performance color highlighter
-		"norcalli/nvim-colorizer.lua",
-		config = function()
-			require("colorizer").setup()
-		end,
-	},
+    {
+        "nvim-treesitter/playground",
+    },
+    {
+        -- Tmux & split window navigation
+        "christoomey/vim-tmux-navigator",
+    },
+    {
+        -- Powerful Git integration for Vim
+        "tpope/vim-fugitive",
+        config = function()
+            vim.keymap.set("n", "<leader>gs", vim.cmd.Git)
+        end,
+    },
+    {
+        -- GitHub integration for vim-fugitive
+        "tpope/vim-rhubarb",
+    },
+    {
+        -- Hints keybinds
+        "folke/which-key.nvim",
+    },
+    {
+        -- High-performance color highlighter
+        "norcalli/nvim-colorizer.lua",
+        config = function()
+            require("colorizer").setup()
+        end,
+    },
 }
