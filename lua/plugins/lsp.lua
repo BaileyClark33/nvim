6de2eea5534d5970870e4245589a21a1dbd80f06
BaileyClark33 <bwc339@gmail.com>
2025-05-03 19:10:11 -0400

    added gipity and python lsp

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -209,31 +209,60 @@ return {
 				--
 				-- But for many setups, the LSP (`tsserver`) will work just fine
 				-- ts_ls = {}, -- tsserver is deprecated
-				-- ruff = {},
-				-- pylsp = {
-				-- settings = {
-				-- pylsp = {
-				-- plugins = {
-				-- pyflakes = { enabled = false },
-				-- pycodestyle = { enabled = false },
-				-- autopep8 = { enabled = false },
-				-- yapf = { enabled = false },
-				-- mccabe = { enabled = false },
-				-- pylsp_mypy = { enabled = false },
-				-- pylsp_black = { enabled = false },
-				-- pylsp_isort = { enabled = false },
-				-- },
-				-- },
-				-- },
-				-- },
+				ruff = {
+					-- Notes on code actions: https://github.com/astral-sh/ruff-lsp/issues/119#issuecomment-1595628355
+					-- Get isort like behavior: https://github.com/astral-sh/ruff/issues/8926#issuecomment-1834048218
+					commands = {
+						RuffAutofix = {
+							function()
+								vim.lsp.buf.execute_command({
+									command = "ruff.applyAutofix",
+									arguments = {
+										{ uri = vim.uri_from_bufnr(0) },
+									},
+								})
+							end,
+							description = "Ruff: Fix all auto-fixable problems",
+						},
+						RuffOrganizeImports = {
+							function()
+								vim.lsp.buf.execute_command({
+									command = "ruff.applyOrganizeImports",
+									arguments = {
+										{ uri = vim.uri_from_bufnr(0) },
+									},
+								})
+							end,
+							description = "Ruff: Format imports",
+						},
+					},
+				},
+				pylsp = {
+					settings = {
+						pylsp = {
+							plugins = {
+								pyflakes = { enabled = false },
+								pycodestyle = { enabled = false },
+								autopep8 = { enabled = false },
+								yapf = { enabled = false },
+								mccabe = { enabled = false },
+								pylsp_mypy = { enabled = false },
+								pylsp_black = { enabled = false },
+								pylsp_isort = { enabled = false },
+							},
+						},
+					},
+				},
 				-- html = { filetypes = { "html", "twig", "hbs" } },
 				-- cssls = {},
 				-- tailwindcss = {},
-				-- dockerls = {},
-				-- sqlls = {},
-				-- terraformls = {},
-				-- jsonls = {},
-				-- yamlls = {},
+				jsonls = {},
+				sqlls = {},
+				terraformls = {},
+				yamlls = {},
+				bashls = {},
+				dockerls = {},
+				docker_compose_language_service = {},
 
 				lua_ls = {
 					-- cmd = {...},
