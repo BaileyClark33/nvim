9ffbaba0ca32565e00bf6bbf364c270a126acf84
BaileyClark33 <bwc339@gmail.com>
2025-05-05 19:00:55 -0400

    added <A-l> for x mode

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -57,8 +57,8 @@ vim.keymap.set("n", "<leader>f", vim.lsp.buf.format)
 
 vim.keymap.set("n", "<C-k>", "<cmd>cnext<CR>zz")
 vim.keymap.set("n", "<C-j>", "<cmd>cprev<CR>zz")
-vim.keymap.set({ "n", "v" }, "<A-h>", "_")
-vim.keymap.set({ "n", "v" }, "<A-l>", "$")
+vim.keymap.set({ "n", "v", "x" }, "<A-h>", "_")
+vim.keymap.set({ "n", "v", "x" }, "<A-l>", "$")
 vim.keymap.set("n", "<leader>k", "<cmd>lnext<CR>zz")
 vim.keymap.set("n", "<leader>j", "<cmd>lprev<CR>zz")
 
