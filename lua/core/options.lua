026740ee59f92a593c3a298c3d14ac68b6905b5b
BaileyClark33 <bwc339@gmail.com>
2025-05-07 19:16:47 -0400

    EOF no characters

--- lua/core/options.lua
+++ lua/core/options.lua
@@ -17,7 +17,7 @@ vim.opt.smartindent = true
 vim.opt.wrap = false
 vim.opt.linebreak = true
 
-vim.opt.fillchars = { eob = ">" } -- Change ~ to >
+vim.opt.fillchars = { eob = " " } -- Change ~ to >
 
 vim.opt.swapfile = false
 vim.opt.backup = false
