8714899a76758f404c0078c96e43a5d0778a945d
BaileyClark33 <bwc339@gmail.com>
2025-05-16 19:32:43 -0400

    hardcoded jdtls package paths

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,6 +1,7 @@
 return {
 	{
 		"jay-babu/mason-nvim-dap.nvim",
+        dependencies = { "williamboman/mason.nvim" },
 		config = function()
 			require("mason-nvim-dap").setup({
 				ensure_installed = { "java-debug-adapter", "java-test" },
